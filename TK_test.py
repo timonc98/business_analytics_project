d36cffc296912f73cc6ee4cd65c97c83b02d327a
Lukas Ziegler <Ziegler.Lukas@gmx.net>
2022-11-16 14:01:56 +0100

    test 5

--- TK_test.py
+++ TK_test.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from tkinter import ttk
-
+######## Lukas ######## 
 Window = Tk()
 Window.geometry("900x500")
 Window.configure(background='lightblue')
@@ -13,4 +13,10 @@ ttk.Button(frm, text="Quit", command=Window.destroy).grid(column=1, row=0)
 ttk = Text(frm, height = 5, width = 52).grid(column=2, row=0)
 Window.mainloop()
 
-test timon
+######## Timon ########
+
+
+######## Alex ########
+
+
+######## Marcel ########
