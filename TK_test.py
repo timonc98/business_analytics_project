24847821fdfbb1623a28691229a550b94f48be75
Lukas Ziegler <Ziegler.Lukas@gmx.net>
2022-11-16 14:19:00 +0100

    Test7

--- TK_test.py
+++ TK_test.py
@@ -2,19 +2,24 @@
 
 from tkinter import *
 from tkinter import ttk
-### Imports
+### Functions
+
+def Window_Zwei():
+    athletic = Tk()
+    athletic.geometry("900x500")
+    athletic.mainloop()
 
 ######## Lukas ######## 
 Window = Tk()
 Window.geometry("900x500")
-Window.configure(background='lightblue')
+Window.configure(background='lightgrey')
 
-frm = ttk.Frame(Window, padding=10)
+frm = ttk.Frame(Window, padding=10,)
 
 frm.grid()
 ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-ttk.Button(frm, text="Quit", command=Window.destroy).grid(column=1, row=0)
-ttk = Text(frm, height = 5, width = 52).grid(column=2, row=0)
+ttk.Button(frm, text="Quit", command=Window_Zwei).grid(column=1, row=0)
+Text(frm, height = 5, width = 52).grid(column=2, row=0)
 Window.mainloop()
 
 ######## Timon ########
