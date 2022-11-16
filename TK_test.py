aa1596011d0b780b714e85c2fbb9f44a55e244d7
Lukas Ziegler <Ziegler.Lukas@gmx.net>
2022-11-16 13:51:55 +0100

    Test3

--- TK_test.py
+++ TK_test.py
@@ -1,9 +1,14 @@
 from tkinter import *
 from tkinter import ttk
 
-root = Tk()
-frm = ttk.Frame(root, padding=10)
+Window = Tk()
+Window.geometry("900x500")
+Window.configure(background='lightblue')
+
+frm = ttk.Frame(Window, padding=10)
+
 frm.grid()
 ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
-root.mainloop()
+ttk.Button(frm, text="Quit", command=Window.destroy).grid(column=1, row=0)
+ttk = Text(frm, height = 5, width = 52).grid(column=2, row=0)
+Window.mainloop()
