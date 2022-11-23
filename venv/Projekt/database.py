0eef5ec87c8bbd8470f1cb54e2b2c8f5778d60df
Lukas Ziegler <Ziegler.Lukas@gmx.net>
2022-11-23 11:24:06 +0100

    Datenbank aufrufen 1

--- venv/Projekt/database.py
+++ venv/Projekt/database.py
@@ -1,34 +1,35 @@
 import psycopg2
 
-con = psycopg2.connect(database="postgres", user="postgres", password="timon1998", host="127.0.0.1", port="5432")
+con = psycopg2.connect(database="postgres", user="postgres", password="Lukasso007", host="127.0.0.1", port="5432")
 
 print("Database opened successfully")
 
-# CREATE TABLE ATHELTE
-# cur = con.cursor()
-# cur.execute('''CREATE TABLE ATHLETE
-#       (ADMISSION INT PRIMARY KEY     NOT NULL,
-#       NAME           TEXT    NOT NULL,
-#       AGE            INT     NOT NULL,
-#       SIZE           CHAR (50),
-#       WEIGHT         CHAR(50),
-#       GENDER         TEXT     NOT NULL);''')
-# print("Table created successfully")
-
-# CREATE TABLE TRAINING
+#CREATE TABLE ATHELTE
 cur = con.cursor()
-cur.execute('''CREATE TABLE TRAINING
-      (TNr INT PRIMARY KEY     NOT NULL,
-      DESIGNATION      TEXT    NOT NULL);''')
+cur.execute('''CREATE TABLE IF NOT EXISTS ATHLETE 
+      (Text_Athlete_Name TEXT       NOT NULL,
+      Text_Athlete_Weight      INT    NOT NULL  );''')
 print("Table created successfully")
 
+# CREATE TABLE TRAINING
+# cur = con.cursor()
+# cur.execute('''CREATE TABLE TRAINING
+#       (TNr INT PRIMARY KEY     NOT NULL,
+#       DESIGNATION      TEXT    NOT NULL);''')
+# print("Table created successfully")
+
 con.commit()
-con.close()
+
 
 # cur = con.cursor()
 
 # cur.execute("INSERT INTO ATHLETE (ADMISSION,NAME,AGE,SIZE,WEIGHT,GENDER) VALUES (3, 'Jakob', '22' ,186, 80, 'Männlich')");
+Name = "Lukas"
+Weight = 120
 
+cur.execute("INSERT INTO ATHLETE (Text_Athlete_Name,Text_Athlete_Weight) VALUES (%s,%s)",(Name, Weight))
+con.commit()
+con.close()
 # con.commit()
 # print("Record inserted successfully")
 # con.close()
